a068e14899877c141e51e04b3876ac8f731bca67
Vinicius Melo <vmelo7289@gmail.com>
2025-08-25 01:23:35 -0300

    terceiro commit

--- components/intro.tsx
+++ components/intro.tsx
@@ -75,7 +75,7 @@ export default function Intro(){
 
                 <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
                 focus:scale-110 hover:scale-105 hover:bg-gray-300 active:scale-105 transition cursor-pointer border border-black/10' 
-                href= "/CV.pdf" download={true}>
+                href= "/CV.png" download={true}>
                     Download CV <HiDownload className='opacity=60
                     group-hover:translate-y-1 transition'/>
                 </a>
